f83abb1aa08260cf157e11fbcab3fbcb587f41b8
jarry <acbbc70412@gmail.com>
2023-05-17 09:24:19 +0800

    git init

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ const Home = async ({searchParams} : Props) => {
   return (
     <ClientOnly>
       <Container>
-        <div className=' pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+        <div className=' pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gpa-8'>
           {listings.map((item) => {
             return (
               <ListingCard key={item.id} data={item} currentUser= {currentUser}/>
